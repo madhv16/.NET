d5e3e07b9df327b94a2b6ca95e07c7faaa170e47
Madhav <madhavgs1012@gmail.com>
2025-09-09 10:26:28 +0530

    ok

---
+++ firstweb/firstweb/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using firstweb.Models; // We MUST include this to use our Product class
+//using System.Collections.Generic;
+
+//namespace firstweb.Controllers
+//{
+//    public class ProductsController : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            List<Product> products = new List<Product>
+//            {
+//                new Product{ Id = 1, Name = "Laptop", Price = 12000.0m
+//                },
+//                new Product{Id = 2,Name = "Mouse",Price = 120.0m }
+//                };
+
+//                return View(products);
+//        }
+//    }
+//}
+
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Data;   
+using firstweb.Models; 
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace MyFirstWebApp.Controllers 
+{
+    public class ProductsController : Controller
+    {
+       
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
+        
+        public IActionResult Index()
+        {
+           
+            List<Product> products = _context.Products.ToList();
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost] 
+        [ValidateAntiForgeryToken] 
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+    }
+}
---
+++ firstweb/firstweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using firstweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MyFirstWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+       
+        public DbSet<Product> Products { get; set; }
+    }
+}
