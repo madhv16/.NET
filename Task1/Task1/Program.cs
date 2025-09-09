5ebb11a4c10173cf1192a7799b9fa45d60132ed9
Madhav G S <85487315+madhv16@users.noreply.github.com>
2025-09-09 09:39:49 +0530

    Add files via upload

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+Console.Write("enter your age:");
+int age = int.Parse(Console.ReadLine());
+
+
+Console.Write($"Hi {name},");
+Console.WriteLine($"Your age is:{age}");
+
